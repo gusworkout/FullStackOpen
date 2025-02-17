6394fb83f923f5b0f0aaf93cbbc4271be2bcff3b
gustavo <tavoaod@gmail.com>
2025-02-17 00:17:56 -0500

    part1_1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,6 +2,66 @@ import { useState } from 'react'
 
 
 const App = () => {
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.'
+]
+
+const [selected, setSelected] = useState(0)
+const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+const HandleNextButton = () =>{
+  const random = Math.floor(Math.random() * anecdotes.length)
+  setSelected(random)
+  
+}
+
+const HandleVoteButton = () =>{
+  const copy = [...votes]
+  copy[selected] += 1
+  setVotes(copy)
+  console.log(copy)
+}
+
+return (
+  <div>
+    <h1>Anecdote of the day</h1>
+    {anecdotes[selected]}
+    <br />
+    has {votes[selected]} votes 
+    <Button onClick={HandleNextButton} text={'Next anecdote'}/>
+    <Button onClick={HandleVoteButton} text={'Vote'}/>
+    <h1>Anecdote with most votes</h1>
+    {anecdotes[votes.indexOf(Math.max(...votes))]}
+  </div>
+)
+
+}
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+
+
+
+
+
+export default App
+
+
+
+ /*const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -71,19 +131,8 @@ const Statistics = ({good, neutral, bad, all}) => {
         Positive: {good/all} %
       </p>
     </div>
-  )
-}
-
-
-
-
-
-
-export default App
-
-
-
- /* const App = () => {
+  ) 
+    const App = () => {
   const [left, setLeft]=useState(0)
   const [right, setRight]=useState(0)
   const [allClicks, setAll] = useState([])
